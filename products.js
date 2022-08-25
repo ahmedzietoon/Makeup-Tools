f9e392b534774383f1fa9cfa06f8caf873e8d0fe
Ahmed Saleh <49107666+ahmedzietoon@users.noreply.github.com>
2022-08-25 23:15:05 +0200

    Update products.js

--- products.js
+++ products.js
@@ -5,7 +5,7 @@ const productType = urlParams.get("product_type"); //after ?product_type=..
 
 const getProducts = async () => {
   const response = await fetch(
-    `http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${productType}`
+    `https://makeup-api.herokuapp.com/api/v1/products.json?product_type=${productType}`
   );
   return await response.json();
 };
